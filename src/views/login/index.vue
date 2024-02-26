4b1989210f7d8d56cde07d37bcb93883219a6ce1
jiege <1768669274@qq.com>
2024-02-26 15:43:44 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import router from "@/router";
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+const Logins = async () => {
+  await userStore.login({ phoneNumber: "15201956179", password: 123456 });
+  router.push('/')
+};
+</script>
+<template>
+  登录页
+  <button @click="Logins">登录</button>
+</template>
+
+<style scoped lang="less"></style>
